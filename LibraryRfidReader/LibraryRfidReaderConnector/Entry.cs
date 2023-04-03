c38f4f08adcc4e94b288b744630fbccf8bef8997
Roman <roman.grabichler@gema-technik.de>
2023-04-03 13:11:02 +0200

    Library und Example zusammengelegt

---
+++ LibraryRfidReader/LibraryRfidReaderConnector/Entry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Vortex.Adapters.Connector.Tc3.Adapter;
+
+namespace LibraryRfidReader
+{
+    public class Entry
+    {
+        public static readonly string AmsId = "5.109.28.160.1.1";
+
+        public static LibraryRfidReaderTwinController Plc { get; } =
+            new LibraryRfidReaderTwinController(Tc3ConnectorAdapter.Create(AmsId, 851, true));
+    }
+}
---
+++ LibraryRfidReader/LibraryRfidReaderExampleConnector/Entry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Vortex.Adapters.Connector.Tc3.Adapter;
+
+namespace LibraryRfidReaderExample
+{
+    public class Entry
+    {
+        public static readonly string AmsId = "5.109.28.160.1.1";
+
+        public static LibraryRfidReaderExampleTwinController Plc { get; } =
+            new LibraryRfidReaderExampleTwinController(Tc3ConnectorAdapter.Create(AmsId, 852, true));
+    }
+}
